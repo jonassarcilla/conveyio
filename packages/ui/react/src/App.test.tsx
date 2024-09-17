6d6661f81f66153a4b224fa5fd6f6c5499fb5150
Jonas Rafael Sarcilla <jr.sarcilla07@gmail.com>
2024-09-17 21:11:57 +0800

    initial commit

---
+++ packages/ui/react/src/App.test.tsx
@@ -0,0 +1,25 @@
+// App.test.tsx
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import App from './App';
+
+// ? INFO (serif): ABOUT TEST WRITING STEPS
+// ! 1. Arrange : render the component under test
+// ! 2. Act : get the element to test
+// ! 3. Assert : check the element is in the document
+
+// ? CHECK (serif) : THE COMMON MISTAKE -> https://kentcdodds.com/blog/common-mistakes-with-react-testing-library
+
+describe('App', () => {
+    it('should render the title', () => {
+        // Arrange : render the component under test
+        render(<App />);
+
+        // Act : get the element to test
+
+        // Assert
+        expect(screen.getByText('Hello, world!')).toBeInTheDocument();
+        expect(screen.getByTestId('app-wrapper')).toBeInTheDocument();
+    });
+});
